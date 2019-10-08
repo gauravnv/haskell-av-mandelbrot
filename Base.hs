81e7ccdd8b0ddf2b5048f0a61160562789276d4b
nmorriss7109 <cybermancan@gmail.com>
2019-10-07 23:11:15 -0700

    now zooming kinda works

--- Base.hs
+++ Base.hs
@@ -1,19 +1,49 @@
-import Graphics.Gloss
-import Graphics.Gloss.Juicy
 import Codec.Picture
-import Codec.Picture.RGBA8
-import Data.Complex
+import Codec.Picture.Types
+import System.Environment
 
-width = 512
-height = width `div` 2
+main :: IO ()
+main = do
+    putStrLn "Beginning rendering of image."
+    makenframes 1 100
 
-mandelbrotness :: Complex Float -> Complex Float -> Float
-mandelbrotness c s = (fromIntegral $ length $ takeWhile (\c -> magnitude c <= 2) $ take maxIter $ z c) / (fromIntegral maxIter) where
-	z c = iterate (\x -> x^2 + c) s
-	maxIter = 32
+makenframes :: Int -> Int -> IO ()
+makenframes n end
+ 							| n >= end = putStrLn "Completed all"
+							| otherwise    = do
+																savePngImage ("frames\\"++(show n)++".jpg") (generateFractal n)
+																putStrLn ("Completed"++(show n))
+																makenframes (n+1) end
+offset :: (Float, Float)
+offset = (0.1, 0.9)
 
-image :: Float -> Image PixelRGB8
-image time = generateImage (\x y -> let [x',y',w,h] = map fromIntegral [x,y,width,height] in mandelbrotColor ((4*x'/w - 2.5) :+ (2*y'/h - 1)) (0.1*time :+ 0) ) width height where
-	mandelbrotColor c s = PixelRGB8 (floor $ 255 * mandelbrotness c s) 120 120
+zoomfactor :: Float
+zoomfactor = 0.75
 
-main = animate (InWindow "Mandelbrot" (width,height) (0,0)) black (fromImageRGB8 . image)
+width :: Int
+width = 200
+
+height :: Int
+height = 100
+
+generateFractal :: Int -> DynamicImage
+generateFractal n = ImageRGB8 $ (generateImage (mandelbrot n)) width height
+
+iters :: Int
+iters = 25
+
+palette :: [PixelRGB8]
+palette = foldr (\a -> \b -> (PixelRGB8 (fromIntegral a*15) (fromIntegral a*10) 5):b) [] [0..iters]
+
+mandelbrot :: Int -> Int -> Int -> PixelRGB8
+mandelbrot n x0 y0 = getColor 0 0 0 n
+    where getColor :: Float -> Float -> Int -> Int -> PixelRGB8
+          getColor x y i n = if x*x + y*y < 2*2 && i < iters
+                           then getColor (x*x - y*y + (scaleX x0 n)) (2*x*y + (scaleY y0 n)) (i+1) n
+                           else palette!!i
+
+scaleX :: Int -> Int -> Float
+scaleX x n = (3.5/ (zoomfactor*(fromIntegral n)+1) * (fromIntegral x)) / (fromIntegral width) - fst offset-- - zoomfactor * (fromIntegral n))
+
+scaleY :: Int -> Int -> Float
+scaleY y n = (2/ (zoomfactor*(fromIntegral n)+1) * (fromIntegral y)) / (fromIntegral height) - snd offset-- - zoomfactor * (fromIntegral n))
