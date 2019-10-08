b2d10e07098fc40d0e0211407fcc027849e75d2c
nmorriss7109 <cybermancan@gmail.com>
2019-10-08 02:32:12 -0700

    Finished rendering 400x300 demo, made gifs!

--- Base.hs
+++ Base.hs
@@ -15,35 +15,35 @@ makenframes n end
 																putStrLn ("Completed"++(show n))
 																makenframes (n+1) end
 offset :: (Float, Float)
-offset = (0.1, 0.9)
+offset = (0.099, 0.89398)
 
 zoomfactor :: Float
-zoomfactor = 0.75
+zoomfactor = 0.03
 
 width :: Int
-width = 200
+width = 400
 
 height :: Int
-height = 100
+height = 300
 
 generateFractal :: Int -> DynamicImage
 generateFractal n = ImageRGB8 $ (generateImage (mandelbrot n)) width height
 
-iters :: Int
-iters = 25
+iters :: Int -> Int
+iters n = 25 + n*n
 
-palette :: [PixelRGB8]
-palette = foldr (\a -> \b -> (PixelRGB8 (fromIntegral a*15) (fromIntegral a*10) 5):b) [] [0..iters]
+palette :: Int -> [PixelRGB8]
+palette n = foldr (\a -> \b -> (PixelRGB8 (fromIntegral a*10) (fromIntegral a*5) (fromIntegral a*2)):b) [] [0..(iters n)]
 
 mandelbrot :: Int -> Int -> Int -> PixelRGB8
 mandelbrot n x0 y0 = getColor 0 0 0 n
     where getColor :: Float -> Float -> Int -> Int -> PixelRGB8
-          getColor x y i n = if x*x + y*y < 2*2 && i < iters
+          getColor x y i n = if x*x + y*y < 2*2 && i < (iters n)
                            then getColor (x*x - y*y + (scaleX x0 n)) (2*x*y + (scaleY y0 n)) (i+1) n
-                           else palette!!i
+                           else (palette n)!!i
 
 scaleX :: Int -> Int -> Float
-scaleX x n = (3.5/ (zoomfactor*(fromIntegral n)+1) * (fromIntegral x)) / (fromIntegral width) - fst offset-- - zoomfactor * (fromIntegral n))
+scaleX x n = (3.5/ (zoomfactor*(fromIntegral (n*n*n)))) * (fromIntegral x) / (fromIntegral width) - fst offset-- - zoomfactor * (fromIntegral n))
 
 scaleY :: Int -> Int -> Float
-scaleY y n = (2/ (zoomfactor*(fromIntegral n)+1) * (fromIntegral y)) / (fromIntegral height) - snd offset-- - zoomfactor * (fromIntegral n))
+scaleY y n = (2/ (zoomfactor*(fromIntegral (n*n*n)))) * (fromIntegral y) / (fromIntegral height) - snd offset-- - zoomfactor * (fromIntegral n))
